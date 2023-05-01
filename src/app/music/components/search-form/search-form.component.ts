62d42a52bff23197215febfb85f38ddf98340c13
Kamil Stępkowski <kamilst18@gmail.com>
2023-05-01 12:18:12 +0200

    Query params in Search URL (q).

--- src/app/music/components/search-form/search-form.component.ts
+++ src/app/music/components/search-form/search-form.component.ts
@@ -1,17 +1,17 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Album } from '../../model/Album';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.scss']
+  styleUrls: ['./search-form.component.scss'],
 })
 export class SearchFormComponent {
   @Output() searchClickChange = new EventEmitter<Album['name']>();
 
-  query: Album['name'] = ''
+  @Input() query: Album['name'] = '';
 
   searchClickHandler() {
-    this.searchClickChange.emit(this.query)
+    this.searchClickChange.emit(this.query);
   }
 }
--- src/app/music/containers/album-search-view/album-search-view.container.ts
+++ src/app/music/containers/album-search-view/album-search-view.container.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MusicApiService } from 'src/app/core/services/music-api/music-api.service';
-import { Album, AlbumSearchResponse } from '../../model/Album';
+import { Album } from '../../model/Album';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-album-search-view',
@@ -8,21 +10,55 @@ import { Album, AlbumSearchResponse } from '../../model/Album';
   styleUrls: ['./album-search-view.container.scss'],
 })
 export class AlbumSearchViewContainer {
-  constructor(@Inject(MusicApiService) private service: MusicApiService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    @Inject(MusicApiService) private service: MusicApiService
+  ) {}
 
   results: Album[] = [];
   message = '';
+  query = '';
+  sub = new Subscription();
+
+  ngOnInit(): void {
+    const queryChanges = this.route.queryParamMap.pipe(
+      map((qpm) => qpm.get('q')),
+      filter(Boolean)
+    );
+
+    this.sub.add(
+      queryChanges
+        .pipe(switchMap((query) => this.service.fetchAlbumSearchResults(query)))
+        .subscribe({
+          next: (albums) => {
+            this.results = albums;
+          },
+          error: (err) => {
+            this.message = err.message;
+          },
+        })
+    );
+
+    queryChanges.subscribe((query) => {
+      this.query = query;
+    });
+    
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   search(query: string) {
-    this.message = ''
+    // reset error
+    this.message = '';
 
-    this.service.fetchAlbumSearchResults(query)
-    .subscribe({
-      next: (albums) => {
-        this.results = albums
-      },
-      error: (err) => {
-        this.message = err.message
+    this.router.navigate([], {
+      relativeTo: this.route,
+
+      queryParams: {
+        q: query,
       },
     });
   }
